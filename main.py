a58e961cae3d7296b8020b25c6afeedc2dd4aae3
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-04-13 15:21:16 -0400

    added test endpoint

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+resp = requests.get('https://api.tdameritrade.com/v1/marketdata/$DJI/movers')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    print('{} {}'.format(todo_item['id'], todo_item['summary']))
